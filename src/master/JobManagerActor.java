6f77cc96d484e226adcf63cb1a25f34f46b582a1
Hassan Nazeer Chaudhry <HASSANNAZEER.CHAUDHRY@POLIMI.IT>
2022-01-14 01:00:17 +0500

    Added boiler plate for Job Manager

--- src/master/JobManagerActor.java
+++ src/master/JobManagerActor.java
@@ -1,6 +1,7 @@
 package master;
 
 import akka.actor.AbstractActorWithStash;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -8,15 +9,28 @@ import shared.PropertyHandler;
 import shared.messages.*;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class JobManagerActor extends AbstractActorWithStash {
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
     public Boolean directedEdges = true;
 
 
+
+    /** Ref to slave and associated number of threads  */
+    private final Map<ActorRef, Integer> slaves = new HashMap<>();
+
+
+    private final Set<ActorRef> clients = new HashSet<>();
+
+
+
     @Override
     public Receive createReceive() {
-        log.info("Creating JobMamager");
+        log.info("Creating JobManager");
         try {
             directedEdges = PropertyHandler.getProperty("directedEdges").equals("true");
 
@@ -46,13 +60,22 @@ public class JobManagerActor extends AbstractActorWithStash {
 
     private final void onHelloClientMsg(HelloClientMsg msg){
         log.info(msg.toString());
-
-
+        this.clients.add(sender());
     }
+
 
     private final void onSlaveAnnounceMsg(SlaveAnnounceMsg msg) {
         log.info(msg.toString());
 
+        slaves.put(getSender(), msg.numThreads);
+        try {
+            int maxSize = Integer.parseInt(PropertyHandler.getProperty("numberOfSlaves")) * Integer.parseInt(PropertyHandler.getProperty("numOfWorkers"));
+            if (slaves.size() == maxSize) {
+                this.onLaunchMsg(new LaunchMsg());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
